cc54a7749b41fa79060ab960cbc51a4637ba6684
justindlangley <justindlangley@gmail.com>
2015-04-17 16:16:38 -0400

    Created seed method

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -8,7 +8,18 @@ using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ContosoUniversity.DAL
 {
-    public class SchoolContext
+    public class SchoolContext : DbContext
     {
+        public SchoolContext () : base ("SchoolContext")
+        {
+        }
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Enrollment> Enrollments { get; set; }
+            public DbSet<Course> Courses { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Conventions.Remove<PluralizingTableNameConvention> () ;
+            }
     }
 }
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -9,7 +9,7 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
         public string LastName { get; set; }
-        public string FristMidName { get; set; }
+        public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
