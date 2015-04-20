07351cdf0f20eb9a8089073dce745aa29749b976
justindlangley <justindlangley@gmail.com>
2015-04-20 14:43:49 -0400

    Added pages

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -17,10 +17,20 @@ namespace ContosoUniversity.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Student
-        public ActionResult Index(string sortOrder, string searchString)
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
+            ViewBag.CurrentSort = sortOrder;
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date"; 
+            
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else { searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
             var students = from s in db.Students
                            select s;
             if (!String.IsNullOrEmpty(searchString))
@@ -44,8 +54,10 @@ namespace ContosoUniversity.Controllers
                 students = students.OrderBy(s => s.LastName);
                 break;
             }
-        
-            return View(students.ToList());
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(students.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: Student/Details/5
