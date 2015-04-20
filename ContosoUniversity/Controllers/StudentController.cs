5719269705b5e14eca9076d61b2b0248d6ae49f5
justindlangley <justindlangley@gmail.com>
2015-04-20 10:58:34 -0400

    Modified student controller

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -105,11 +105,15 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Student/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id, bool? saveChangesError=false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed.  Try again, and if the problem persists, contact your system administrator.";
             }
             Student student = db.Students.Find(id);
             if (student == null)
