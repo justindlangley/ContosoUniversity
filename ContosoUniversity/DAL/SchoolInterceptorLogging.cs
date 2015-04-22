b6e52059acbec046811424ef6b01f96bd669a2d9
justindlangley <justindlangley@gmail.com>
2015-04-22 09:40:39 -0400

    Created SchoolInterceptorLogging

---
+++ ContosoUniversity/DAL/SchoolInterceptorLogging.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Data.Entity.SqlServer;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Reflection;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.Logging;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolInterceptorLogging
+    {
+    }
+}
