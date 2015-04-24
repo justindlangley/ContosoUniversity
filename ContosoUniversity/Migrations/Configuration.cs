3838d430ff0c80b3bb00b44b2ac2f0868e43c3d4
justindlangley <justindlangley@gmail.com>
2015-04-24 16:07:59 -0400

    Config.Cs

--- ContosoUniversity/Migrations/Configuration.cs
+++ ContosoUniversity/Migrations/Configuration.cs
@@ -166,5 +166,12 @@ namespace ContosoUniversity.Migrations
             //    );
             //
         }
+        void AddOrUpdateInstructor(SchoolContext context, string courseTitle, string instructorName)
+        {
+            var crs = context.Courses.SingleOrDefault(c => c.Title == courseTitle);
+            var inst = crs.Instructors.SingleOrDefault(i => i.LastName == instructorName);
+            if (inst==null)
+                crs.Instructors.Add(context.Instructors.Single(i=>i.LastName == instructorName));
+        }
     }
 }
